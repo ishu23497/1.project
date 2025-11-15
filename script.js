f8806b6a2325448594caa427e90604247feb8fc6
ishu <royalmansingh98@gmail.com>
2025-11-15 11:38:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,115 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets){
+    // move small cursor
+    crsr.style.left = dets.clientX + "px";
+    crsr.style.top = dets.clientY + "px";
+
+    // move blur cursor slightly behind for smooth trailing effect
+    blur.style.left = dets.clientX - 200 + "px";
+    blur.style.top = dets.clientY - 200 + "px";
+});
+
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height:"110px",
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        markers:true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
+    }
+});
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers:true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2
+    }
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+
+h4all.forEach(function(elem) {
+
+  elem.addEventListener("mouseenter", function() {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+
+  elem.addEventListener("mouseleave", function() {
+    gsap.to("#cursor", {
+      scale: 1,
+      border: "0px solid #95C11E",
+      backgroundColor: "#95C11E",
+      duration: 0.3
+    });
+  });
+
+});
+
+gsap.from("#about-us img, #about-us-in", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: "#about-us",
+        scroller: "body",
+        markers:true,
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 3
+    }
+});
+gsap.from(".card", {
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.4,
+    scrollTrigger: {
+        trigger: ".card",
+        scroller: "body",
+        markers:true,
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1
+    }
+});
+
+gsap.from("#colon1", {
+     y: -70,
+     x: -70,
+     scrollTrigger: {
+            trigger: "#colon1",
+            scroller: "body",
+            markers:true,
+            start: "top 60%",
+            end: "top 55%",
+            scrub: 4
+        },
+});
+gsap.from("#colon2", {
+     y: 70,
+     x: 70,
+     scrollTrigger: {
+            trigger: "#colon1",
+            scroller: "body",
+            markers:true,
+            start: "top 60%",
+            end: "top 55%",
+            scrub: 4
+        },
+});
